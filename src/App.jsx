987b5fcc798f0628b268680b0be0e40d6dfb2dcc
atiqurrr <atiqurbdrahman2965@gmail.com>
2025-04-14 21:33:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    //1.Login/Logout Toggle
+    //{isLoggedIn ? <LogoutButton /> : <LoginButton />}
+
+    <div>
+      <h1>All ternary oparetors</h1>
+    </div>
+  );
+};
+
+export default App;
